140679f4678da07e5c4e274cb675a1a212c57124
sonirathore01 <sonirathore01@gmail.com>
2017-01-27 17:35:44 +0530

    code commit
    get,post,put,delete apis of node.js  called from angular.js
     and succeccfully operated all methods on database

--- demo3.js
+++ demo3.js
@@ -12,7 +12,7 @@ var app = express();
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/mydb'); // connect to our database
-var Emp = require('./db/emp');
+var Emp = require('./db/employee');
 
 app.set('token', '1234 ');
 app.use(bodyParser.json());
@@ -22,13 +22,13 @@ app.use(cookieParser());
 app.use(cors());
 
 app.get('/get', function (req, res) {
-    Emp.find({}, {_id: 0, __v: 0}, function (err, data) {
+    Emp.find({}, {__v: 0}, function (err, data) {
         if (err)
             return res.send(err);
 
         return res.json(data);
     });
-})
+});
 
 app.post('/register', function (req, res) {
     if (!req.body.name)
@@ -38,24 +38,49 @@ app.post('/register', function (req, res) {
     emp.department = req.body.department;
     emp.username = req.body.username;
     emp.password = req.body.password;
-    Emp.find(function (err, data) {
+    emp.save(function (err) {
+        if (err)
+            return res.status(409).send(err);
+
+        return res.json({message: 'employee registered successfully !!!'});
+    });
+});
+
+app.put('/update/:sid', function (req, res) {
+    if (!req.body.name)
+        return res.status(400).send({error: 'Name is required'});
+
+    Emp.findById(req.params.sid, function (err, data) {
+
         if (err) {
             return res.send(err);
-        } else {
-            if (data.length > 0) {
-                emp.emp_id = data.length + 1;
-            } else {
-                emp.emp_id = 1;
-            }
-            emp.save(function (err) {
-                if (err)
-                    return res.status(409).send(err);
 
-                return res.json({message: 'employee registered successfully !!!'});
+            data.name = req.body.name;  // update the bears info
+            data.department = req.body.department;
+            data.username = req.body.username;
+            data.password = req.body.password;
+            // data.marks = req.body.marks;  // update the bears info
+            // save the bear
+            data.save(function (err) {
+                if (err)
+                    res.send(err);
+
+                res.json({message: 'Bear updated!'});
             });
         }
     });
-})
+});
+
+app.delete('/delete/:sid', function (req, res) {
+    Emp.remove({
+        _id: req.params.sid
+    }, function (err, stud) {
+        if (err)
+            res.send(err);
+
+        res.json({message: 'Successfully deleted'});
+    });
+});
 
 app.post('/login', function (req, res) {
     console.log("data:", req.body.username);
@@ -77,8 +102,7 @@ app.post('/login', function (req, res) {
             }
         }
     });
-})
-
+});
 
 var server = app.listen(8081, function () {
 
@@ -86,5 +110,4 @@ var server = app.listen(8081, function () {
     var port = server.address().port;
 
     console.log("Server started at http://%s:%s", host, port)
-});
-
+})
