c6c91c7349bc4f142bcfbd5451661cb2d3a56c40
Soni Rathore <sonirathore01@gmail.com>
2017-02-07 18:00:23 +0530

    task-2 given by sir

--- mlm/client/controllers/CustController.js
+++ mlm/client/controllers/CustController.js
@@ -1,11 +1,10 @@
-/**
- * Created by lcom73 on 31/1/17.
- */
 angular
     .module('app')
     .controller('CustController', CustController);
 
-function CustController($http,$window,Upload) {
+CustController.$inject = ['$scope', '$http', 'Upload'];
+
+function CustController($scope,$http,$window,Upload,$modal) {
     var BASE_API = 'http://localhost:8081/';
     var vm = this;
     getCust();
@@ -16,7 +15,6 @@ function CustController($http,$window,Upload) {
     $http
         .get(BASE_API + 'getparent')
         .then(function (data) {
-            console.log("p:",data);
             vm.parents = data.data;
         }), (function (err) {
     });
@@ -50,9 +48,88 @@ function CustController($http,$window,Upload) {
             })
     }
 
+    vm.delete = function (id) {
+        console.log(id);
+        $http
+            .delete(BASE_API + 'delete/' + id)
+            .then(function (res) {
+                // getCust();
+                // console.log(res);
+                vm.newCust = {};
+            }, function (err) {
+                console.log(err);
+            })
+    };
+
     vm.sort = function(keyname){
         vm.sortKey = keyname;   //set the sortKey to the param passed
-        vm.reverse = !$scope.reverse; //if true make it false and vice versa
+        vm.reverse = !vm.reverse; //if true make it false and vice versa
     }
+
+    vm.editForm = function (id) {
+        vm.message = "Edit Form Button Clicked";
+        console.log(vm.message);
+
+        var modalInstance = $modal.open({
+            templateUrl: 'edit.html',
+            controller: ModalInstanceCtrl,
+            scope: $scope,
+            resolve: {
+                userForm: function () {
+                    return $scope.userForm;
+                }
+            }
+        });
+    }
+
+    vm.deleteForm = function (id) {
+        vm.message = "Delete Form Button Clicked";
+        console.log(vm.message);
+
+        var modalInstance = $modal.open({
+            templateUrl: 'delete.html',
+            controller: ModalInstanceCtrl,
+            scope: $scope,
+            resolve: {
+                userForm: function () {
+                    return $scope.userForm;
+                }
+            }
+        });
+    }
+
+    var ModalInstanceCtrl = function ($scope, $http, $modalInstance, userForm, Upload) {
+
+        ModalInstanceCtrl.$inject = ['Upload', '$http']
+
+        $scope.delete = function () {
+            $http({
+                url: '/delete/' + $scope._id,
+                method: 'DELETE'
+            })
+            $modalInstance.dismiss();
+            $scope.getCust();
+        }
+
+        $scope.update = function () {
+            Upload.upload({
+                url: '/update/' + $scope._id,
+                method: 'PUT',
+                data: $scope.newCust
+
+            }).then(function (response) {
+
+                $scope.Cust = response.data
+                $modalInstance.dismiss();
+                $scope.getCust();
+                $scope.newCust = {};
+            })
+
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    };
 
 }
